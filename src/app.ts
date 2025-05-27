eff223760cb41bad0b1a209d4ea600e5d28556a6
gustavo_polonio <gustavopolonio1@gmail.com>
2025-05-26 21:19:47 -0300

    feat(config): add env validation with ZOD

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,8 @@
-import express from 'express'
+import { env } from './env';
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-  console.log('Response sent!')
-})
-
-app.listen(process.env.PORT ?? '9001', () => {
-  console.log(`App listening on port: ${process.env.PORT ?? '9001'}`)
-})
+app.listen(env.PORT, () => {
+  console.log(`App listening on port: ${env.PORT}`);
+});
