485fd89a0be435cf38ff8ab256e65668f92c4bf7
gustavo_polonio <gustavopolonio1@gmail.com>
2025-06-03 21:42:30 -0300

    feat(route): create route to unbookmark project

--- src/routes/projectsRoutes/index.ts
+++ src/routes/projectsRoutes/index.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response } from "express";
 import z from "zod";
 import axios from "axios";
+import { Prisma } from "generated/prisma";
 import { prisma } from "@/lib/prisma";
 import { GitHubRepo } from "@/@types/github";
 import { env } from "@/env";
-import { Prisma } from "generated/prisma";
 
 export const projectsRoutes = Router();
 
@@ -126,6 +126,57 @@ projectsRoutes.post(
         res
           .status(409)
           .json({ message: "Project already bookmarked by this user" });
+      }
+
+      if (error instanceof Error) {
+        res.status(400).send({ message: error });
+        return;
+      }
+      res.status(500).send({ message: "Unknown error" });
+      return;
+    }
+  }
+);
+
+projectsRoutes.delete(
+  "/:projectId/bookmark",
+  async (req: Request, res: Response) => {
+    const deleteBookmarkParamsSchema = z.object({
+      projectId: z.coerce.number(),
+    });
+
+    const { projectId } = deleteBookmarkParamsSchema.parse(req.params);
+
+    const { userId } = req.user;
+
+    try {
+      const project = await prisma.project.findUnique({
+        where: {
+          id: projectId,
+        },
+      });
+
+      if (!project) {
+        res.status(404).send({ message: "Project not found" });
+        return;
+      }
+
+      await prisma.bookmark.delete({
+        where: {
+          userId_projectId: { userId, projectId },
+        },
+      });
+
+      res.status(200).json({ message: "Project bookmark removed" });
+      return;
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      ) {
+        res
+          .status(409)
+          .json({ message: "Project is not bookmarked by this user" });
       }
 
       if (error instanceof Error) {
