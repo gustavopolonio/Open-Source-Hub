37651985c9f92df825a59457ef7c3250df9b40ac
gustavo_polonio <gustavopolonio1@gmail.com>
2025-06-07 18:41:44 -0300

    feat: create signup component

---
+++ frontend/src/routes/login.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Typography } from "@/components/ui/typography";
+import { createFileRoute, Link } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/login")({
+  component: Login,
+});
+
+function Login() {
+  return (
+    <div className="h-[calc(100vh-64px)] flex flex-col items-center justify-center max-w-lg mx-auto py-16 space-y-10">
+      <Typography variant="h1" className="text-center">
+        Log in to
+        <span className="block">Open Source Hub</span>
+      </Typography>
+      <Button size="lg" variant="secondary" className="w-80 font-bold" asChild>
+        <Link to="https://github.com/login/oauth/authorize?client_id=Iv23lihzv6YIdLtC83FE">
+          Continue with GitHub
+        </Link>
+      </Button>
+      <Typography variant="p">
+        Don't have an account?{" "}
+        <Button asChild variant="link" className="p-0">
+          <Link to="/signup">Sign Up</Link>
+        </Button>
+      </Typography>
+    </div>
+  );
+}
