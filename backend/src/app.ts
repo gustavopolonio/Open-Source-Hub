f787d84ffc2331a5a0501cb6cc8a17e1033c3439
gustavo_polonio <gustavopolonio1@gmail.com>
2025-06-10 19:41:41 -0300

    feat: config cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import { env } from "./env";
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { verifyJwt } from "./middlewares/verify-jwt";
 import { usersRoutes } from "./routes/usersRoutes";
 import { githubRoutes } from "./routes/githubRoutes";
@@ -9,6 +10,11 @@ import { getPublicProjects, projectsRoutes } from "./routes/projectsRoutes";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: env.FRONTEND_BASE_URL,
+  })
+);
 
 app.use("/", usersRoutes);
 app.use("/github", verifyJwt, githubRoutes);
