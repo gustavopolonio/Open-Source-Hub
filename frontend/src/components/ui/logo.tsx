1d4057d4c97895be8e998e3ba813dc8a8b0790fb
gustavo_polonio <gustavopolonio1@gmail.com>
2025-07-22 20:38:44 -0300

    minor fix: logo size

--- frontend/src/components/ui/logo.tsx
+++ frontend/src/components/ui/logo.tsx
@@ -1,10 +1,5 @@
 import { DoorOpen } from "lucide-react";
-import { Button } from "./button";
 
 export function Logo() {
-  return (
-    <Button size="icon" variant="ghost">
-      <DoorOpen className="w-full! h-auto!" />
-    </Button>
-  );
+  return <DoorOpen className="w-9 h-auto" />;
 }
