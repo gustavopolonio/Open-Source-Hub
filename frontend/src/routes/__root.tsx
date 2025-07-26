1d4a234d67609822bec0339f4a5f6e790a1feafb
gustavo_polonio <gustavopolonio1@gmail.com>
2025-07-26 10:06:16 -0300

    feat: create Not Found page

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,9 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import type { AuthContextType } from "@/context/AuthProvider";
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { PageContainer } from "@/components/layout/PageContainer";
-import type { AuthContextType } from "@/context/AuthProvider";
+import { Typography } from "@/components/ui/typography";
 
 export const Route = createRootRouteWithContext<AuthContextType>()({
   component: () => (
@@ -13,5 +14,15 @@ export const Route = createRootRouteWithContext<AuthContextType>()({
       </PageContainer>
       <Footer />
     </>
+  ),
+  notFoundComponent: () => (
+    <div className="flex items-center justify-center">
+      <Typography variant="h1" className="text-2xl pr-6 mr-6 border-r">
+        404
+      </Typography>
+      <Typography variant="h2" className="text-sm">
+        This page could not be found.
+      </Typography>
+    </div>
   ),
 });
