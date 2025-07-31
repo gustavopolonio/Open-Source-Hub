d574647d3b3bcbba770d8cf43ea2886759b52711
gustavo_polonio <gustavopolonio1@gmail.com>
2025-07-31 10:36:37 -0300

    fix: set trust proxy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,6 +10,9 @@ import { apiRoutes } from "./routes/api.routes";
 import { globalLimiter } from "./middlewares/rate-limiters";
 
 const app = express();
+
+app.set("trust proxy", 1);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
